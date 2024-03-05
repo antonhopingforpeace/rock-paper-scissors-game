54830c2d757fed87efc583f1f2980d3d8fc2e255
antonhopingforpeace <116687553+antonhopingforpeace@users.noreply.github.com>
2024-03-05 18:51:04 +0200

    Complete the whole game. No html details, no CSS, only Javascript

---
+++ javascript.js
@@ -0,0 +1,119 @@
+function getComputerChoice(){
+
+    const myNumber = Math.floor(Math.random()*3)+1;
+
+    switch(myNumber){
+        case 3:
+            return "Rock";
+        case 2:
+            return "Paper";
+        case 1:
+            return "Scissors";
+    }
+
+}
+
+function playRound(playerSelection, computerSelection){
+
+    const player1 = playerSelection.toLowerCase();
+    const player2 = computerSelection.toLowerCase();
+
+    if (player1===player2){
+        console.log("Its a tie, you both brought "+player1);
+        return 0;
+    }
+    else if (player1 === "rock" && player2==="paper"){
+        console.log("You Lose! Paper beats Rock");
+        return -1;
+    }
+    else if (player1 === "rock" && player2==="scissors"){
+        console.log("You Win! Rock beats Scissors");
+        return 1;
+    }
+    else if (player1 === "paper" && player2==="scissors"){
+        console.log("You Lose! Scissors beats Paper");
+        return -1;
+    }
+    else if (player1 === "paper" && player2==="rock"){
+        console.log("You Win! Paper beats Rock");
+        return 1;
+    }
+    else if (player1 === "scissors" && player2==="paper"){
+        console.log("You Win! Scissors beats Paper");
+        return 1;
+    }
+    else if(player1 === "scissors" && player2==="rock"){
+        console.log("You Lose! Rock beats Scissors");
+        return -1;
+    }
+    else{
+        console.log("Your input was wrong, give a answer between ROCK, PAPER and SCISSORS")
+        return -2;
+    }
+
+}
+
+function showResult(player1,player2){
+    console.log("Player score:"+player1);
+    console.log("Computer score:"+player2);
+}
+
+function showWinner(player1,player2){
+    if(player1>player2){
+        console.log("You win, congratulations!!!");
+    }
+    else if(player2>player1){
+        console.log("You lost for the computer, unlucky");
+    }
+    else{
+        console.log("Not a bad game, but its a tie.")
+    }
+}
+
+function playGame(){
+
+    let computerScore=0;
+    let playerScore=0;
+
+    for(let round=1; round<=5; round++){
+
+        const computerSelection = getComputerChoice();
+        let playerSelection = prompt("Choose between rock,paper,scissors:");
+        let result = playRound(playerSelection,computerSelection);
+        
+        if(result===1){
+            playerScore++;
+        }
+        else if(result===-1){
+            computerScore++;
+        }
+        else if(result===0){
+            computerScore++;
+            playerScore++;
+        }
+        else{
+            
+            while(result===-2){
+                let playerSelection = prompt("Choose between rock,paper,scissors:");
+                result = playRound(playerSelection,computerSelection);
+                if(result===1){
+                    playerScore++;
+                }
+                else if(result===-1){
+                    computerScore++;
+                }
+                else if(result===0){
+                    computerScore++;
+                    playerScore++;
+                }   
+            }
+        }
+        
+        console.log("ROUND"+round+":");
+        showResult(playerScore,computerScore);
+    }
+
+    showWinner(playerScore,computerScore);
+}
+
+playGame();
