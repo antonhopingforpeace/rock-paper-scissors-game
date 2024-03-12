e5021ca864edc2f5173eca5006782874be55e839
antonhopingforpeace <116687553+antonhopingforpeace@users.noreply.github.com>
2024-03-12 17:10:36 +0200

    Create 3 buttons and display message for winner

--- javascript.js
+++ javascript.js
@@ -20,35 +20,43 @@ function playRound(playerSelection, computerSelection){
 
     if (player1===player2){
         console.log("Its a tie, you both brought "+player1);
-        return 0;
+        newDiv.textContent="Its a tie, you both brought "+player1;
+        //return 0;
     }
     else if (player1 === "rock" && player2==="paper"){
         console.log("You Lose! Paper beats Rock");
-        return -1;
+        newDiv.textContent="You Lose! Paper beats Rock";
+        //return -1;
     }
     else if (player1 === "rock" && player2==="scissors"){
         console.log("You Win! Rock beats Scissors");
-        return 1;
+        newDiv.textContent="You Win! Rock beats Scissors";
+        //return 1;
     }
     else if (player1 === "paper" && player2==="scissors"){
         console.log("You Lose! Scissors beats Paper");
-        return -1;
+        newDiv.textContent="You Lose! Scissors beats Paper";
+        //return -1;
     }
     else if (player1 === "paper" && player2==="rock"){
         console.log("You Win! Paper beats Rock");
-        return 1;
+        newDiv.textContent="You Win! Paper beats Rock";
+        //return 1;
     }
     else if (player1 === "scissors" && player2==="paper"){
         console.log("You Win! Scissors beats Paper");
-        return 1;
+        newDiv.textContent="You Win! Scissors beats Paper";
+        //return 1;
     }
     else if(player1 === "scissors" && player2==="rock"){
         console.log("You Lose! Rock beats Scissors");
-        return -1;
+        newDiv.textContent="You Lose! Rock beats Scissors";
+        //return -1;
     }
     else{
         console.log("Your input was wrong, give a answer between ROCK, PAPER and SCISSORS")
-        return -2;
+        newDiv.textContent="Your input was wrong, give a answer between ROCK, PAPER and SCISSORS";
+        //return -2;
     }
 
 }
@@ -70,50 +78,70 @@ function showWinner(player1,player2){
     }
 }
 
-function playGame(){
+// function playGame(){
 
-    let computerScore=0;
-    let playerScore=0;
+//     let computerScore=0;
+//     let playerScore=0;
 
-    for(let round=1; round<=5; round++){
+//     for(let round=1; round<=5; round++){
 
-        const computerSelection = getComputerChoice();
-        let playerSelection = prompt("Choose between rock,paper,scissors:");
-        let result = playRound(playerSelection,computerSelection);
+//         const computerSelection = getComputerChoice();
+//         let playerSelection = prompt("Choose between rock,paper,scissors:");
+//         let result = playRound(playerSelection,computerSelection);
         
-        if(result===1){
-            playerScore++;
-        }
-        else if(result===-1){
-            computerScore++;
-        }
-        else if(result===0){
-            computerScore++;
-            playerScore++;
-        }
-        else{
+//         if(result===1){
+//             playerScore++;
+//         }
+//         else if(result===-1){
+//             computerScore++;
+//         }
+//         else if(result===0){
+//             computerScore++;
+//             playerScore++;
+//         }
+//         else{
             
-            while(result===-2){
-                let playerSelection = prompt("Choose between rock,paper,scissors:");
-                result = playRound(playerSelection,computerSelection);
-                if(result===1){
-                    playerScore++;
-                }
-                else if(result===-1){
-                    computerScore++;
-                }
-                else if(result===0){
-                    computerScore++;
-                    playerScore++;
-                }   
-            }
-        }
+//             while(result===-2){
+//                 let playerSelection = prompt("Choose between rock,paper,scissors:");
+//                 result = playRound(playerSelection,computerSelection);
+//                 if(result===1){
+//                     playerScore++;
+//                 }
+//                 else if(result===-1){
+//                     computerScore++;
+//                 }
+//                 else if(result===0){
+//                     computerScore++;
+//                     playerScore++;
+//                 }   
+//             }
+//         }
         
-        console.log("ROUND"+round+":");
-        showResult(playerScore,computerScore);
-    }
+//         console.log("ROUND"+round+":");
+//         showResult(playerScore,computerScore);
+//     }
 
-    showWinner(playerScore,computerScore);
-}
+//     showWinner(playerScore,computerScore);
+// }
 
-playGame();
+// playGame();
+
+let buttonRock = document.getElementById("rock");
+let buttonPaper = document.getElementById("paper");
+let buttonScissors = document.getElementById("scissors");
+
+let body = document.querySelector("body");
+
+let newDiv = document.createElement("div");
+
+
+
+buttonRock.addEventListener("click", ()=> {
+    playRound("rock",getComputerChoice())});
+buttonPaper.addEventListener("click", ()=> {
+    playRound("paper",getComputerChoice())});
+buttonScissors.addEventListener("click", ()=>{
+    playRound("scissors",getComputerChoice())});
+
+body.appendChild(newDiv);
+
